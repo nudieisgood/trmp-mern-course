fd27218bbe1767eea91f8f817bf3c479d1afe6c4
nudieisgood <nudieisgoodmii@gmail.com>
2023-08-09 16:02:47 +0800

    fix upload image

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
 import Job from "../models/JobModel.js";
 import cloudinary from "cloudinary";
-import { promises as fs } from "fs";
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -26,17 +26,16 @@ export const updateUser = async (req, res) => {
   delete newUser.password;
 
   if (req.file) {
-    const res = await cloudinary.v2.uploader.upload(req.file.path);
-    console.log(res);
+    const file = formatImage(req.file);
+    const res = await cloudinary.v2.uploader.upload(file);
+    // const res = await cloudinary.v2.uploader.upload(req.file.path);
     //將利用muter store在 public/uploads 的file delete
-    await fs.unlink(req.file.path);
+    // await fs.unlink(req.file.path);
     //加入avatar property before update new user to database
     newUser.avatar = res.secure_url;
     newUser.avatarPublicId = res.public_id;
   }
-
   const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser);
-
   //如果此次update 有上傳新file (req.file is true) 我們要將舊的file從cloudinary刪除
   //updateUser.avatarPublicId 若存在代表之前有上傳過file, 若無表示是第一次update file
   //我們只做了create user 的動作
